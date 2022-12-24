328f31c024c9acedf81297c761a4b3840a13fc96
Abdelhakim RAFIK <a.rafik_etu@enset-media.ac.ma>
2022-12-25 00:12:14 +0100

    fix: resolve commit issue

---
+++ matriculation-service/src/main/java/com/abdelhakimrafik/matriculationservice/MatriculationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abdelhakimrafik.matriculationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatriculationServiceApplication.class, args);
+    }
+
+}
---
+++ matriculation-service/src/test/java/com/abdelhakimrafik/matriculationservice/MatriculationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdelhakimrafik.matriculationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
